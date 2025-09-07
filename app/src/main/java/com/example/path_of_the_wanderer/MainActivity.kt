98cb50e4e5b349b7988642fea30d90b98827e8e2
Daniel Balobalo <70095157+Daniel-Balobalo@users.noreply.github.com>
2025-09-08 05:42:28 +0800

    9/8/2025 Commit

--- app/src/main/java/com/example/path_of_the_wanderer/MainActivity.kt
+++ app/src/main/java/com/example/path_of_the_wanderer/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PathoftheWandererTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Daniel",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
